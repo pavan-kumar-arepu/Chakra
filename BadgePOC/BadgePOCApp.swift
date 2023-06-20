8131f98b3c1e66a9d1a4f9b646cd29caef216670
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2023-06-20 22:57:01 +0200

    Initial Commit

---
+++ BadgePOC/BadgePOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadgePOCApp.swift
+//  BadgePOC
+//
+//  Created by Pavankumar Arepu on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BadgePOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
